64fb3a386b0a93447082132ba2ba336081096038
samuv <verzi@nweb.it>
2016-03-07 18:43:49 +0100

    add logic for on/off presets

--- src/components/presets.js
+++ src/components/presets.js
@@ -8,11 +8,13 @@ angular.module('MyApp')
     templateUrl: '/components/presets.html',
     controller: function($scope, $element, $attrs) {
       $scope.presets = Presets.presets();
+      $scope.current = ''
 
       $scope.choose = function(name) {
         Presets
           .choose(name)
           .then(function(p) {
+            $scope.current = name
             $scope.preset = p;
           });
       };
